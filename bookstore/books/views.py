26934d6711fe06517ce7d51aca01c4556c98b28d
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-02-16 15:00:30 +0800

    show index

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from books.models import Books
+from books.enums import *
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator
 
 # Create your views here.
+
